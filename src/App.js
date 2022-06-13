97e27248e3aca676008ddf494cdb20f255fbb962
laurentdong@163.com <laurentdong@163.com>
2022-06-13 19:52:49 +0800

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo, Suspense } from "react";
+import { renderRoutes } from "react-router-config";
 
-function App() {
+import routers from "./router";
+import { HashRouter } from "react-router-dom";
+
+import HYAppHeader from "components/app-header";
+import HYAppFooter from "components/app-footer";
+
+export default memo(function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <HYAppHeader />
+      {renderRoutes(routers)}
+      <HYAppFooter />
+    </HashRouter>
   );
-}
-
-export default App;
+});
---
+++ src/pages/friend/index.js
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+
+const HYFriend = memo(() => {
+  return <div>HYFriend</div>;
+});
+
+export default HYFriend;
